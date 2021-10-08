4fcb2fa274278d1d0af8a6b6a9165f1302ddf141
Javier Legua <javierlegua14@gmail.com>
2021-10-08 10:35:43 +0200

    Trabajo

---
+++ DESARROLLO DE APLICACIONES ENTORNO CLIENTE/prueba.js
@@ -0,0 +1,13 @@
+alert("Hello World!")
+
+var respuesta;
+respuesta = confirm("¿Desea cancelar la subscripción?");
+alert ("Usted ha contestado que "+respuesta);
+
+var provincia;
+provincia = prompt("Introduzca la provincia","Teruel");
+alert("Usted ha introducido la siguiente información "+provincia);
+
+var nombre;
+nombre = prompt("Dime tu nombre","Javier");
+alert("Su nombre es "+nombre);
